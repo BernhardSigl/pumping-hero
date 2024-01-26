bb0b2d3f1b8bdd6230446ef7fe59ac0b2f6b6a9f
Bernhard Sigl <b-sigl@live.de>
2024-01-26 20:12:11 +0100

    add imprint and privacy policy

--- src/app/main-content/settings/settings.component.ts
+++ src/app/main-content/settings/settings.component.ts
@@ -1,21 +1,42 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../../models/auth.service';
+import { ImprintComponent } from '../imprint/imprint.component';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { PrivacyPolicyComponent } from '../privacy-policy/privacy-policy.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
   imports: [
-    MatButtonModule
+    CommonModule,
+    MatButtonModule,
+    ImprintComponent,
+    MatIconModule,
+    PrivacyPolicyComponent
   ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+  currentContent = 'buttons';
   auth = inject(AuthService);
 
   signOut() {
     sessionStorage.removeItem('loggedInUser');
     this.auth.signOut();
+  }
+
+  showButtons() {
+    this.currentContent = 'buttons';
+  }
+
+  showImprint() {
+    this.currentContent = 'imprint';
+  }
+
+  showPrivacyPolicy() {
+    this.currentContent = 'privacy-policy';
   }
 }
