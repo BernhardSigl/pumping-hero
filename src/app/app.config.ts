a52da0297bb22eb9309a7793dc5f1b6db9ff25d6
Bernhard Sigl <b-sigl@live.de>
2024-06-16 19:02:14 +0200

    fix app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,16 +11,24 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideServiceWorker } from '@angular/service-worker';
 
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "simplefit-411215", "appId": "1:48091826759:web:bcd9bc0bcf27597e41bf62", "storageBucket": "simplefit-411215.appspot.com", "apiKey": "AIzaSyDCKYbFyzDhKgeMf1PXbNp1EEmtbQnqK2Q", "authDomain": "simplefit-411215.firebaseapp.com", "messagingSenderId": "48091826759" }))), importProvidersFrom(provideFirestore(() => getFirestore())), provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    }), provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    }), provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    })]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'simplefit-411215',
+        appId: '1:48091826759:web:bcd9bc0bcf27597e41bf62',
+        storageBucket: 'simplefit-411215.appspot.com',
+        apiKey: 'AIzaSyDCKYbFyzDhKgeMf1PXbNp1EEmtbQnqK2Q',
+        authDomain: 'simplefit-411215.firebaseapp.com',
+        messagingSenderId: '48091826759',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 };
